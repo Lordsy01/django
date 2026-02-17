a90bacafcf48930421262389bb946001bb4c9fd9
Lordsy <lordsyntokungwia@gmail.com>
2026-02-17 07:32:33 -0800

    resolving circular relationships

--- store/models.py
+++ store/models.py
@@ -8,23 +8,24 @@ class promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=200)
-
+    featured_product= models.ForeignKey('Product', 
+    on_delete=models.SET_NULL, null=True, related_name='+')
 class Product(models.Model):
-    MEMBERSHIP_BRONZE='B'
-    MEMBERSHIP_SILVER='S'
-    MEMBERSHIP_GOLD='G'
+    # MEMBERSHIP_BRONZE='B'
+    # MEMBERSHIP_SILVER='S'
+    # MEMBERSHIP_GOLD='G'
 
-    MEMBERSHIP_CHOICES=[
-        ('MEMBERSHIP_BRONZE', 'Bronze'),
-        ('MEMBERSHIP_SILVER', 'Silver'),
-        ('MEMBERSHIP_GOLD', 'Gold')
-    ]
+    # MEMBERSHIP_CHOICES=[
+    #     ('MEMBERSHIP_BRONZE', 'Bronze'),
+    #     ('MEMBERSHIP_SILVER', 'Silver'),
+    #     ('MEMBERSHIP_GOLD', 'Gold')
+    # ]
     title = models.CharField(max_length=255)
     discription = models.Textfield()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory= models.IntergerField()
     last_update = models.DateTimeField(auto_now=True)
-    collection=models.ForeignKey(collection, on_delete=models.PROTECT)
+    collection=models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
@@ -33,11 +34,11 @@ class Customer(models.Model):
     email=models.EmailField(unique=True, max_length=200)
     phone=models.CharField(max_length=200)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default-'MEMBERSHIP_BRONZE')
+    # membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default-'MEMBERSHIP_BRONZE')
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING ='P'
-    PAYMENT-STATUS_COMPLETE='C'
+    PAYMENT_STATUS_COMPLETE='C'
     PAYMENT_STATUS_FAILED = 'F'
 
     PAYMENT_STATUS_CHOICES=[
